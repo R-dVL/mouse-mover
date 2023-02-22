52c4c62f4124312901f6aa2b4cd5a1f80e895c57
Raúl Del Valle Lima <raul.del.valle.lima@kyndryl.com>
2023-02-22 17:02:37 +0100

    Mejoras y .bat

---
+++ app/Move Mouse.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\GitHub\Move-Mouse\Move_Mouse\Scripts & activate & cd /d    C:\GitHub\Move-Mouse\app & python main.py"
--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ import pyautogui
 import time
 
 def moverRaton():
-    pyautogui.dragRel(0, 100, duration = 1)
-    time.sleep(30)
-    pyautogui.dragRel(0,-100, duration = 1)
-    time.sleep(30)
+    while True:
+        print("Parriba..")
+        pyautogui.dragRel(-100, -100, duration = 1)
+        time.sleep(30)
+        print("Pabajo..")
+        pyautogui.dragRel(100, 100, duration = 1)
+        time.sleep(30)
 
 if __name__ == "__main__":
-    print("Taluego...")
-    while True:
+    print("Trae pa' ca' el raton...")
+    try:
         moverRaton()
+    except:
+        print("Taluego!")
+    finally:
+        exit()
