7f5e56b8c10ed4302ad325797d60849bcc2f7063
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-12-22 13:04:41 +0100

    Change dragmouse to keytoggle capslock

--- cmd/stay_active/main.go
+++ cmd/stay_active/main.go
@@ -2,26 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/go-vgo/robotgo"
 )
 
-func stayActive() {
+func StayActive() {
 	for {
-		fmt.Println("Parriba..")
-		x1, y1 := robotgo.Location()
-		robotgo.DragSmooth(x1 - 100, y1 - 100)
-		time.Sleep(30 * time.Second)
-
-		fmt.Println("Pabajo..")
-		x2, y2 := robotgo.Location()
-		robotgo.DragSmooth(x2 + 100, y2 + 100)
-		time.Sleep(30 * time.Second)
+		robotgo.KeyToggle("capslock", "down")
 	}
 }
 
 func main() {
-	fmt.Println("Trae pa' ca' el ratón...")
-	stayActive()
+	fmt.Println("Active...")
+	StayActive()
 }
