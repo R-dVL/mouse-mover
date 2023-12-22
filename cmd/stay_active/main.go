2cad040791e29be423cfe9546010fcfc75ed64fa
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-12-22 13:17:16 +0100

    Improvements

--- cmd/stay_active/main.go
+++ cmd/stay_active/main.go
@@ -8,11 +8,16 @@ import (
 
 func StayActive() {
 	for {
+		fmt.Println("Still here..")
 		robotgo.KeyToggle("capslock", "down")
+		robotgo.MilliSleep(1000)
+		robotgo.KeyToggle("capslock", "up")
+		robotgo.KeyToggle("capslock", "down")
+		robotgo.MilliSleep(60000)
 	}
 }
 
 func main() {
-	fmt.Println("Active...")
+	fmt.Println("Hey, I'm here..")
 	StayActive()
 }
