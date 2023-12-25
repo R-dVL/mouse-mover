893e10764d19e33f7656587baddc373e174e15d4
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2023-12-25 12:24:56 +0100

    Add interactive Menu and options

--- cmd/stay_active/main.go
+++ cmd/stay_active/main.go
@@ -2,22 +2,96 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"time"
 
 	"github.com/go-vgo/robotgo"
 )
 
-func StayActive() {
+func menu() int {
+	var option int
+
+	fmt.Print(`
+	==============================================================
+		________________  __   ___   _________________    ________
+	   / ___/_  __/   \ \/ /  /   | / ____/_  __/  _/ |  / / ____/
+	   \__ \ / / / /| |\  /  / /| |/ /     / /  / / | | / / __/
+	  ___/ // / / ___ |/ /  / ___ / /___  / / _/ /  | |/ / /___
+	 /____//_/ /_/  |_/_/  /_/  |_\____/ /_/ /___/  |___/_____/
+
+	==============================================================
+
+	Choose your option:
+	-------------------
+	1. Work.
+	2. Gaming.
+	3. Exit.
+
+	`)
+
+	fmt.Scan(&option)
+
+	return option
+}
+
+func work() {
 	for {
 		fmt.Println("Still here..")
-		robotgo.KeyToggle("capslock", "down")
-		robotgo.MilliSleep(1000)
-		robotgo.KeyToggle("capslock", "up")
-		robotgo.KeyToggle("capslock", "down")
+		robotgo.KeyTap("capslock")
+		robotgo.KeyTap("capslock")
 		robotgo.MilliSleep(60000)
 	}
 }
 
+func gaming() {
+	for {
+		// Generate random seed everytime
+		rand.Seed(time.Now().UnixNano())
+		action := rand.Intn(4)
+
+		switch action {
+		case 0:
+			fmt.Println("Move forward")
+			robotgo.KeyToggle("w", "down")
+			robotgo.MilliSleep(1000)
+			robotgo.KeyToggle("w", "up")
+			robotgo.MilliSleep(60000)
+		case 1:
+			fmt.Println("Move backwards")
+			robotgo.KeyToggle("s", "down")
+			robotgo.MilliSleep(1000)
+			robotgo.KeyToggle("s", "up")
+			robotgo.MilliSleep(60000)
+		case 2:
+			fmt.Println("Action 1")
+			robotgo.KeyTap("1")
+			robotgo.MilliSleep(60000)
+		case 3:
+			fmt.Println("Action 2")
+			robotgo.KeyTap("2")
+			robotgo.MilliSleep(60000)
+		default:
+			fmt.Println("Not an option.")
+		}
+
+	}
+}
+
 func main() {
-	fmt.Println("Hey, I'm here..")
-	StayActive()
+	for {
+		var option int = menu()
+
+		switch option {
+		case 1:
+			work()
+		case 2:
+			gaming()
+		case 3:
+			fmt.Println("Exiting..")
+			os.Exit(0)
+		default:
+			fmt.Println("Not an option.")
+		}
+	}
 }
